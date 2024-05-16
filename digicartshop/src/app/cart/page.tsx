f6d5e2a74d8dbc52249a1b00b26221c2230ad541
rodrigo-belmonte <rodrigo.belmonte@outlook.com>
2024-05-16 01:43:00 -0300

    Criado  Modal de Finalização de COmpra

--- digicartshop/src/app/cart/page.tsx
+++ digicartshop/src/app/cart/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { ProductCardCart } from "../components/_ui/productCardCart/page";
-import { Button } from "flowbite-react";
+import { use, useEffect, useState } from "react";
+import { ProductCardCart } from "../components/cart/productCardCart/page";
+import { Button, Modal } from "flowbite-react";
 import { useCartContext } from "../contexts/CartContext/CartContext";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { BiHappyAlt } from "react-icons/bi";
+import { useRouter } from "next/navigation";
 
 export default function Cart() {
+  const route = useRouter();
   const [productsCart, setproductsCart] = useState<ProductCart[]>([]);
   const [total, settotal] = useState<number>(0);
+  const [openModal, setOpenModal] = useState(false);
   const cart = useCartContext();
 
   useEffect(() => {
@@ -31,20 +36,21 @@ export default function Cart() {
 
   function handleFecharPedido(): void {
     cart.updateState("");
-    
+    setOpenModal(true);
   }
 
   return (
-    <div >
+    <div>
       {productsCart.length <= 0 ? (
-        <div className="flex flex-col items-center p-10 m-10  ">  
-         Seu Carrinho Está Vazio :(
-
-          <a href="/" className="text-blue-500">Volte para a nossa Loja </a>
+        <div className="flex flex-col items-center p-10 m-10  ">
+          Seu Carrinho Está Vazio :(
+          <a href="/" className="text-blue-500">
+            Volte para a nossa Loja{" "}
+          </a>
         </div>
       ) : (
         <div className="flex flex-col items-center  ">
-          <div className="border border-solid rounded-lg p-10 mb-5 lg:w-1/2 md:w-2/3 ">
+          <div className="border border-solid rounded-lg p-5 mb-5 lg:w-1/2 md:w-2/3 w-full ">
             {productsCart.map((productcart) => (
               <ProductCardCart
                 key={productcart.product.id}
@@ -54,12 +60,11 @@ export default function Cart() {
               />
             ))}
           </div>
-          <div className="border border-solid rounded-lg p-5 w-1/2 text-center ">
+          <div className="border border-solid rounded-lg p-5 lg:w-1/2 md:w-2/3  w-full text-center ">
             Total <span className="font-semibold text-sm">R$ </span>
             <span className="font-bold text-2xl">
               {total.toFixed(2).replace(".", ",")}
             </span>
-            
             <div className="mt-5">
               <Button onClick={handleFecharPedido} className="w-full">
                 Fechar Pedido
@@ -68,6 +73,27 @@ export default function Cart() {
           </div>
         </div>
       )}
+      <Modal
+        show={openModal}
+        size="md"
+        onClose={() => route.push("/")}
+        popup
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <BiHappyAlt className="mx-auto mb-4 h-14 w-14 " />
+            <h3 className="mb-5 text-lg font-normal ">
+              Obrigado por comprar com a gente!
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Button color="success" href="/">
+                Voltar a Loja
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
