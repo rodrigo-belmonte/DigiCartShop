0f8fb188db8206272d026c6bcfa2e5c5f7c0dbc6
rodrigo-belmonte <rodrigo.belmonte@outlook.com>
2024-05-16 00:40:57 -0300

    Componente do Produto da home com a opção de visualização e adicionar direto no carrinho pela home;

--- digicartshop/src/app/components/home/productCardHome/page.tsx
+++ digicartshop/src/app/components/home/productCardHome/page.tsx
@@ -1,45 +1,79 @@
-import { Badge, Card } from "flowbite-react";
+"use client";
+
+import { useCartContext } from "@/app/contexts/CartContext/CartContext";
+import { Badge, Button, Card } from "flowbite-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { BiSolidOffer } from "react-icons/bi";
 
 type ProductCardProps = {
   _product: Product;
 };
 export default function ProductCard({ _product }: ProductCardProps) {
+  const route = useRouter();
+  const cart = useCartContext();
+
+
+
+  function AddProdToCart(): void {
+    if (cart.state !== "") {
+      let prodExistsInCart = cart.state.filter(
+        (product: ProductCart) => product.product.id == _product.id
+      )[0];
+      if (prodExistsInCart) {
+        const prodincart = cart.state;
+        prodincart.map((p: ProductCart) =>
+          p.product.id === prodExistsInCart.product.id
+            ? (p.quantity = p.quantity + 1)
+            : (p = p)
+        );
+        cart.updateState(prodincart);
+      } else {
+        cart.updateState([...cart.state, { product: _product, quantity: 1 }]);
+      }
+    } else {
+      cart.updateState([{ product: _product, quantity: 1 }]);
+    }
+  }
 
   return (
-    <Card
-      className="max-w-sm m-10"
-      imgAlt={_product.detail}
-      imgSrc={_product.image}
-    >
-      <a href="#">
-        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-          {_product.name}
-        </h5>
-      </a>
-      <div className="flex ">
-        {_product.offer != undefined ? (  
-          <Badge icon={BiSolidOffer} color="green" className=" justify-center ">
-           <span> {_product.offer}</span>
-          </Badge>
-        ) : (
-          <></>
-        )}
-      </div>
-     
-      
-      <div className="flex items-center justify-between">
+    <div>
+      <Card
+        className="max-w-sm mx-10 border-b-0 rounded-b-none"
+        imgAlt={_product.detail}
+        imgSrc={_product.image}
+        href={`product?id=${_product.id}`}
+      >
+        <div>
+          <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+            {_product.name}
+          </h5>
+        </div>
+        <div className="flex h-14 ">
+          {_product.offer != undefined ? (
+            <Badge
+              icon={BiSolidOffer}
+              color="green"
+              className=" justify-center "
+            >
+              <span> {_product.offer}</span>
+            </Badge>
+          ) : (
+            <div className="h-14"> </div>
+          )}
+        </div>
+        <div>
+        {_product.detail}
+        </div>
+      </Card>
+      <Card className="mx-10 mb-10 flex items-center justify-between border-t-0 rounded-t-none">
         <span className="text-3xl font-bold text-gray-900 dark:text-white">
-          R${_product.price}
+          R${Number.parseInt(_product.price).toFixed(2).replace(".", ",")}
         </span>
-        <a
-          href="#"
-          className="rounded-lg bg-cyan-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
-        >
+        <Button fullSized onClick={AddProdToCart} className="w-full">
           Add to cart
-        </a>
-      </div>
-    </Card>
+        </Button>
+      </Card>
+    </div>
   );
 }
-
